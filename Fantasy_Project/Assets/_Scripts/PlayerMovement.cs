c1a185ac13c94c60dfcd36d38702736a262c4312
Ismael Echegaray <ismael.echegarayms@hotmail.com>
2022-11-15 23:25:41 -0600

    Up and Down Movement
    -Add up and down movement animation to the player
    -Modify PlayerMovement script to add trigger for the animations mentioned before

--- Fantasy_Project/Assets/_Scripts/PlayerMovement.cs
+++ Fantasy_Project/Assets/_Scripts/PlayerMovement.cs
@@ -9,21 +9,62 @@ public class PlayerMovement : MonoBehaviour
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
+    Animator animator;
 
     void Awake()
     {
         myRigidbody = GetComponent<Rigidbody2D>();    
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Move();
+        FlipSprite();
     }
 
     void Move()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x, moveInput.y) * runSpeed;
         myRigidbody.velocity = playerVelocity;
+
+        if (moveInput.y > 0)
+        {
+            animator.SetBool("IsRunningHorizontal", false);
+            animator.SetBool("IsRunningUp", true);
+        }
+        else if (moveInput.y < 0)
+        {
+            animator.SetBool("IsRunningHorizontal", false);
+            animator.SetBool("IsRunningDown", true);
+        }
+        else if (moveInput.y == 0)
+        {
+            animator.SetBool("IsRunningUp", false);
+            animator.SetBool("IsRunningDown", false);
+        }
+
+        if (moveInput.y == 0 && Mathf.Abs(moveInput.x) > 0)
+        {
+            animator.SetBool("IsRunningHorizontal", true);
+        }
+        else if (moveInput.x == 0)
+        {
+            animator.SetBool("IsRunningHorizontal", false);
+        }
+
+
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+
+        if (moveInput.y > 0) { return; }
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(moveInput.x), 1f);
+        }
     }
 
     void OnMove(InputValue value)
